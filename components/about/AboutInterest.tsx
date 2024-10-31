15df0dc20b43912dfccb15147b58e5add1f5a0e0
Jaypou <146738116+Jaypou@users.noreply.github.com>
2024-10-31 00:11:01 -0400

    Update AboutInterest.tsx

--- components/about/AboutInterest.tsx
+++ components/about/AboutInterest.tsx
@@ -11,15 +11,15 @@ export default function AboutInterest() {
       <div className="flex h-full w-full items-center justify-around rounded-lg bg-black/10 text-white shadow-2xl shadow-black backdrop-blur-sm">
         <div className="flex w-[50%] flex-col gap-y-8 py-6 md:py-10 lg:py-12 xl:py-20">
           <p>
-            I'm someone who thrives on challenge and self-improvement. I love
-            taking on new adventures, whether it's exploring new places, hitting
-            the gym, or testing my limits outdoors.
+            I&apos;m someone who thrives on challenge and self-improvement. I
+            love taking on new adventures, whether it&apos;s exploring new
+            places, hitting the gym, or testing my limits outdoors.
           </p>
           <p>
-            I'm also passionate about motorcycles and a big fan of epic worlds
-            like The Lord of the Rings and the Marvel Universe—let's be honest,
-            DC doesn't quite measure up! In my downtime, I enjoy video games,
-            strong coffee, and trips to the cinema.
+            I&apos;m also passionate about motorcycles and a big fan of epic
+            worlds like The Lord of the Rings and the Marvel Universe—let&apos;s
+            be honest, DC doesn&apos;t quite measure up! In my downtime, I enjoy
+            video games, strong coffee, and trips to the cinema.
           </p>
           <p>
             For me, every day is an opportunity to push myself a little further
